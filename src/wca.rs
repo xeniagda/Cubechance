89b28c90fddaeb418a8ef5a5db9557018e00450d
loovjo <jonathan.loov@gmail.com>
2017-09-19 16:41:12 +0200

    ???

--- src/wca.rs
+++ src/wca.rs
@@ -1,7 +1,5 @@
 /*
- *
  * Simple lib to download and parse the WCA results.
- *
  *
  */
 
@@ -14,22 +12,24 @@ use std::io::{Read, Cursor};
 
 const WCA_TSV_URL: &'static str = "https://www.worldcubeassociation.org/results/misc/WCA_export.tsv.zip";
 
+// Downloads and parses the current WCA results.
 pub fn download_wca() {
     println!("Downloading wca...");
+
     match reqwest::get(WCA_TSV_URL) {
         Err(e) => {
             eprintln!("Couldn't download wca: {}", e);
         }
+
         Ok(ref mut resp) => {
-            let mut res = Vec::new();
+            let mut res = Vec::new(); // Resulting ZIP content
             if let Err(e) = resp.read_to_end(&mut res) {
                 println!("Couldn't read to string: {}", e);
                 return;
             }
             println!("Downloaded. Unzipping...");
             
-            let curs = Cursor::new(res);
-            match ZipArchive::new(curs) {
+            match ZipArchive::new(Cursor::new(res)) {
                 Ok(ref mut zip) => {
                     match zip.by_name("WCA_export_Results.tsv") {
                         Ok(ref mut file) => {
